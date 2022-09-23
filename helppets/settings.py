d341e2fb9018339c06c1cec6bfc94e0919e7c9e6
rustingoff <nailunho@gmail.com>
2022-09-23 20:40:49 +0000

    setting

--- helppets/settings.py
+++ helppets/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,9 @@ SECRET_KEY = 'django-insecure-b_074yay^sx@zd*guod_lxew7-25@owtmj4=wx-a-%9*uybbyh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '104.248.30.74',
+]
 
 
 # Application definition
@@ -117,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'webstatic')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
