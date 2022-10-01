fcafaf6b6a16f1a71b071fa37b7031fa93de3e39
rustingoff <nailunho@gmail.com>
2022-10-01 08:32:53 +0000

    last

--- pets/views.py
+++ pets/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'selected/index.html')
+    context = {
+        "data" : [1, 2, 3],
+        "roadmap": [1, 2, 3, 4, 5, 6]
+    }
+    return render(request, 'selected/index.html', context)
